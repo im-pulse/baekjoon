592150b355163dc31c64a3d7d8d9c8fed5ea79fe
im-pulse <87103941+im-pulse@users.noreply.github.com>
2021-07-13 10:42:06 +0900

    백준 11047번 "동전 0" 문제입니다.
    그리디 알고리즘을 이용하여 문제를 풀었습니다.

---
+++ 11047.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[11];
+int main()
+{
+int a,b; //a:종류, b:만들어야하는 가치
+cin>>a>>b;
+ for(int i=0; i<a; i++)
+  { 
+   cin>>arr[i];
+  }
+int ret = 0;
+for (int i = a - 1; i >= 0; i--) 
+{
+  if (b == 0)
+	  break;
+  while (b >= arr[i]) 
+    {
+      b -= arr[i];
+      ret++;
+    }
+ }
+
+  cout << ret;
+  return 0;
+}
