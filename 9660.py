a4596de6e453fe2e2e26b62fd449ac1064790d0f
im-pulse <87103941+im-pulse@users.noreply.github.com>
2021-07-21 18:39:13 +0900

    백준 9660번 "돌 게임 6" 문제입니다.
    이게 왜 골드임 ??????
    게임이론 이용해서 푸는 쉬운 문제였던거 같습니다

---
+++ 9660.py
@@ -0,0 +1,20 @@
+'''
+왜 이 코드는 안되고 아래코드는 되는걸까....????
+사실 아직도 아리송하다
+
+n= int(input())
+
+if n%7==(0 or 2):
+    print('CY')
+else:
+    print('SK')
+'''
+
+n= int(input())
+
+if n%7==0:
+    print('CY')
+elif n%7==2:
+    print('CY')
+else:
+    print('SK')
