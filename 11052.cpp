c5b3f436106188bb5761ffcb357aab1ae08463a6
im-pulse <87103941+im-pulse@users.noreply.github.com>
2021-08-02 19:27:56 +0900

    백준 11052번 "카드 구매하기" 문제입니다.
    DP 문제입니다.

---
+++ 11052.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[1001]= {0};
+int arr[1001]= {0}; 
+
+int main(){
+    int n;
+    cin >> n;
+    
+    for(int i=1;i<=n;i++)
+        cin >> arr[i];
+    
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=i;j++)
+            dp[i] = max(dp[i], dp[i-j] + arr[j]);
+    
+    cout << dp[n] << endl;
+    
+    return 0;
+}
